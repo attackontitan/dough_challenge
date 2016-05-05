4f2b997d6ca3b2c951ac3d993758d74bfa4be65c
attackontitan <letsgogogo321go@gmail.com>
2016-05-05 10:21:23 -0500

    finance and install chartkick and style

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,12 +1,23 @@
 class Stock < ActiveRecord::Base
   validates_uniqueness_of :symbol
 
+
   def self.search(search)
     unless search.blank?
       where("name LIKE ?", "%#{search}%") + where("symbol LIKE ?", "%#{search}%")
-    # else
-    #   Stock.all
+      # else
+      #   Stock.all
     end
   end
+
+  def draw
+
+  end
+
+  def get_data
+    yahoo_client ||= YahooFinance::Client.new
+    yahoo_client.historical_quotes(self.symbol, {start_date: Time::now-(24*60*60*30), end_date: Time::now}) # 30 days worth of data
+  end
+
 
 end
