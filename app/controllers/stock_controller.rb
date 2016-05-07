d6ddfe119e3f9710e948cc7cfccd0bbc9503adcc
attackontitan <letsgogogo321go@gmail.com>
2016-05-07 16:29:20 -0500

    debug stock repetition and add style

--- app/controllers/stock_controller.rb
+++ app/controllers/stock_controller.rb
@@ -6,11 +6,11 @@ class StockController < ApplicationController
     elsif params[:search].nil?
       @start = true
     else
-      @searched = Stock.search(params[:search]).uniq{|x| x.symbol}
-      @query_str = params[:search]
+      @query_str = params[:search].gsub(/\W+/, '')
+      @searched = Stock.search(@query_str)
       unless @searched.nil?
-        @count = @searched.size
-        @searched = (@searched.sort_by &:name).paginate(:page => params[:page], :per_page => 20)
+        @count = @searched.uniq{|x| x.symbol}.size
+        @searched = (@searched.uniq{|x| x.symbol} .sort_by &:name).paginate(:page => params[:page], :per_page => 20)
       end
     end
   end
@@ -18,9 +18,7 @@ class StockController < ApplicationController
   def draw
     @stock = Stock.find params[:id]
     @draw_data = @stock.format_data
-    # @draw_data = [["02-10",1],["02-11",3],["02-12",5],["02-13",8],["02-14",4],["02-15",2]]
     @test_data = @draw_data.to_s
-    # @chart = new Chartkick.LineChart("chart-1", @draw_data);
   end
 
 end
